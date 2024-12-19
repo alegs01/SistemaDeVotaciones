8cc064a64b3bc1790ff8c480eabc91efc917f317
alegs01 <ale.gomez.saez@gmail.com>
2024-12-19 01:07:22 -0300

    UPDATE

--- pf/index.js
+++ pf/index.js
@@ -1,139 +1,117 @@
+// index.js
 let respuestas = [];
 let conteos = [];
+let preguntas = [];
 
 const mostrarPreguntas = (pregunta) => {
-    console.log(pregunta.texto);
-    pregunta.opciones.forEach((opcion, indice) => {
-        console.log(`${indice + 1}  ${opcion}`);
-    });
+  console.log(pregunta.texto);
+  pregunta.opciones.forEach((opcion, indice) => {
+    console.log(`${indice + 1}  ${opcion}`);
+  });
 };
 
 const recibirRespuesta = () => {
-    let respuesta = prompt('Ingresa tu respuesta (solo números del 1 al 4)');
-    respuesta = parseInt(respuesta, 10); 
-    while (isNaN(respuesta) || respuesta < 1 || respuesta > 4) {
-        respuesta = prompt('Respuesta no válida, por favor ingrese nuevamente (solo números del 1 al 4)');
-        respuesta = parseInt(respuesta, 10); 
-    }
-    return respuesta;
+  let respuesta = prompt("Ingresa tu respuesta (solo números del 1 al 4)");
+  respuesta = parseInt(respuesta, 10);
+  while (isNaN(respuesta) || respuesta < 1 || respuesta > 4) {
+    respuesta = prompt(
+      "Respuesta no válida, por favor ingrese nuevamente (solo números del 1 al 4)"
+    );
+    respuesta = parseInt(respuesta, 10);
+  }
+  return respuesta;
 };
 
-const iniciarEncuesta = (preguntas) => {
-    console.clear();
-    respuestas = [];
-    if (conteos.length === 0) {
-        conteos = preguntas.map(() => [0, 0, 0, 0]);
-    }
-    preguntas.forEach((pregunta, indice) => {
-        mostrarPreguntas(pregunta);
-        const respuesta = recibirRespuesta();
-        respuestas.push(respuesta);
-        conteos[indice][respuesta - 1] += 1;
-    });
-    alert('Ha finalizado la encuesta');
-    document.getElementById('btnResultados').style.display = 'inline-block';
-    document.getElementById('btnConteos').style.display = 'inline-block';
+const iniciarEncuesta = () => {
+  console.clear();
+  respuestas = [];
+  if (conteos.length === 0) {
+    conteos = preguntas.map(() => [0, 0, 0, 0]);
+  }
+  preguntas.forEach((pregunta, indice) => {
+    mostrarPreguntas(pregunta);
+    const respuesta = recibirRespuesta();
+    respuestas.push(respuesta);
+    conteos[indice][respuesta - 1] += 1;
+  });
+  alert("Ha finalizado la encuesta");
+  document.getElementById("btnResultados").style.display = "inline-block";
+  document.getElementById("btnConteos").style.display = "inline-block";
 };
 
-const mostrarResultados = (preguntas, respuestas) => {
-    console.log('Resultados de tu encuesta:');
-    preguntas.forEach((pregunta, indice) => {
-        console.log(`${pregunta.texto} - Respuesta: ${pregunta.opciones[respuestas[indice] - 1]}`);
-    });
+const mostrarResultados = () => {
+  console.log("Resultados de tu encuesta:");
+  preguntas.forEach((pregunta, indice) => {
+    console.log(
+      `${pregunta.texto} - Respuesta: ${
+        pregunta.opciones[respuestas[indice] - 1]
+      }`
+    );
+  });
 };
 
-const mostrarConteos = (preguntas, conteos) => {
-    console.clear();
-    console.log('Conteo de votos:');
-    preguntas.forEach((pregunta, indice) => { 
-        console.log(`${pregunta.texto}:`);
-        pregunta.opciones.forEach((opcion, indiceOpcion) => { 
-            console.log(`${opcion}: ${conteos[indice][indiceOpcion]} veces`);
-        });
+const mostrarConteos = () => {
+  console.clear();
+  console.log("Conteo de votos:");
+  preguntas.forEach((pregunta, indice) => {
+    console.log(`${pregunta.texto}:`);
+    pregunta.opciones.forEach((opcion, indiceOpcion) => {
+      console.log(`${opcion}: ${conteos[indice][indiceOpcion]} veces`);
     });
+  });
 };
 
 function openModal() {
-
-    document.getElementById('preguntasModal').style.display = 'block';
-
-};
-
-function closeModal() {
-    document.getElementById('preguntasModal').style.display = 'none';
-};
-
-function siguiente() {
-
-    pregunta = document.getElementsByClassName('pregunta');
-
-    opciones = document.getElementsByClassName('opciones');
-
-
-    preguntas.push({ texto: pregunta, opciones: opciones });
-
+  document.getElementById("preguntasModal").style.display = "block";
 }
 
-// Índice para rastrear el número de pregunta actual
-let preguntaIndex = 1;
+function closeModal() {
+  document.getElementById("preguntasModal").style.display = "none";
+}
 
-// Número máximo de preguntas permitidas
+let preguntaIndex = 1;
 const maxPreguntas = 4;
 
-// Manejador de eventos para el botón 'Siguiente'
-document.querySelector('.continuarModal').addEventListener('click', function() {
-    // Obtener la pregunta y opciones del formulario
-    const preguntaInput = document.querySelector('.pregunta').value;
-    const opcionesInputs = document.querySelectorAll('.opciones');
+const agregarPregunta = () => {
+  const preguntaInput = document.querySelector(".pregunta").value;
+  const opcionesInputs = document.querySelectorAll(".opciones");
 
-    // Validar que todos los campos estén llenos
-    if (preguntaInput === '' || [...opcionesInputs].some(opcion => opcion.value === '')) {
-        alert('Por favor, rellena todos los campos.');
-        return;
-    }
+  if (
+    preguntaInput === "" ||
+    [...opcionesInputs].some((opcion) => opcion.value === "")
+  ) {
+    alert("Por favor, rellena todos los campos.");
+    return;
+  }
 
-    // Añadir la pregunta y opciones al arreglo
-    preguntas.push({
-        texto: preguntaInput,
-        opciones: [...opcionesInputs].map(opcion => opcion.value)
-    });
+  preguntas.push({
+    texto: preguntaInput,
+    opciones: [...opcionesInputs].map((opcion) => opcion.value),
+  });
 
-    // Vaciar los campos de entrada
-    document.querySelector('.pregunta').value = '';
-    opcionesInputs.forEach(opcion => opcion.value = '');
+  document.querySelector(".pregunta").value = "";
+  opcionesInputs.forEach((opcion) => (opcion.value = ""));
 
-    // Actualizar el texto del único <p> con la clase 'preguntasI'
-    const campo = document.querySelector('.preguntasI');
-    campo.textContent = `Pregunta ${preguntaIndex+1}`;
+  const campo = document.querySelector(".preguntasI");
+  campo.textContent = `Pregunta ${preguntaIndex + 1}`;
 
-    // Incrementar el índice para la próxima pregunta
-    preguntaIndex++;
+  preguntaIndex++;
 
-    // Verificar si se ha alcanzado el límite de preguntas
-    if (preguntaIndex > maxPreguntas) {
-        // Ocultar el botón de preguntas y mostrar el botón de iniciar
-        document.getElementById('btnPreguntas').style.display = 'none';
-        document.getElementById('btnIniciar').style.display = 'block';
-        alert('Has añadido todas las preguntas.');
-        closeModal();
-        console.log(preguntas);
-    }
-});
+  if (preguntaIndex > maxPreguntas) {
+    document.getElementById("btnPreguntas").style.display = "none";
+    document.getElementById("btnIniciar").style.display = "block";
+    alert("Has añadido todas las preguntas.");
+    closeModal();
+  }
+};
 
-
-const preguntas = [
-
-
-/*     { texto: "¿Cuál es tu dispositivo tecnológico favorito para el día a día?", opciones: ["Smartphone", "Notebook", "Tablet", "Smartwatch"] },
-    { texto: "¿Cuál es tu red social favorita?", opciones: ["Facebook", "Instagram", "Twitter", "TikTok"] },
-    { texto: "¿Qué plataforma de streaming prefieres para ver películas y series?", opciones: ["Netflix", "Amazon Prime Video", "Disney+", "HBO Max"] },
-    { texto: "¿Cuál es tu género de videojuegos favorito?", opciones: ["Acción/Aventura", "RPG (Role-Playing Game)", "FPS (First-Person Shooter)", "Estrategia"] },
-    { texto: "¿Qué prefieres usar para escuchar música o podcasts?", opciones: ["Auriculares inalámbricos", "Auriculares con cable", "Altavoces Bluetooth", "No suelo utilizar dispositivos de audio"] },
-    { texto: "¿Qué tipo de contenido prefieres ver en plataformas de video (YouTube, TikTok, etc.)?", opciones: ["Educacional", "Entretenimiento", "Vlogs/personal", "Música"] },
-    { texto: "¿Qué tecnología emergente te interesa más?", opciones: ["Inteligencia Artificial", "Realidad Virtual/Aumentada", "Internet de las Cosas (IoT)", "Blockchain"] },
-    { texto: "¿Cuál es tu plataforma preferida para trabajar o estudiar de forma remota?", opciones: ["Zoom", "Microsoft Teams", "Google Meet", "Skype"] } */
-];
-
-document.getElementById('btnIniciar').addEventListener('click', () => iniciarEncuesta(preguntas));
-document.getElementById('btnResultados').addEventListener('click', () => mostrarResultados(preguntas, respuestas));
-document.getElementById('btnConteos').addEventListener('click', () => mostrarConteos(preguntas, conteos));
+document
+  .querySelector(".continuarModal")
+  .addEventListener("click", agregarPregunta);
+document
+  .getElementById("btnIniciar")
+  .addEventListener("click", iniciarEncuesta);
+document
+  .getElementById("btnResultados")
+  .addEventListener("click", mostrarResultados);
+document.getElementById("btnConteos").addEventListener("click", mostrarConteos);
--- poo/index.js
+++ poo/index.js
@@ -1,80 +1,154 @@
 class Pregunta {
-    constructor(texto, opciones) {
-        this.texto = texto;
-        this.opciones = opciones;
-    }
+  constructor(texto, opciones) {
+    this.texto = texto;
+    this.opciones = opciones;
+  }
 
-    mostrarPreguntas() {
-        console.log(this.texto);
-        this.opciones.forEach((opcion, indice) => {
-            console.log(indice + 1, ' ', opcion);
-        });
-    }
+  mostrarPregunta() {
+    console.log(this.texto);
+    this.opciones.forEach((opcion, indice) => {
+      console.log(`${indice + 1}  ${opcion}`);
+    });
+  }
 
-    recibirRespuesta() {
-        let respuesta = prompt('Ingresa tu respuesta (solo números del 1 al 4)');
-        respuesta = parseInt(respuesta, 10); 
-        while (isNaN(respuesta) || respuesta < 1 || respuesta > 4) {
-            respuesta = prompt('Respuesta no válida, por favor ingrese nuevamente (solo números del 1 al 4)');
-            respuesta = parseInt(respuesta, 10); 
-        }
-        return respuesta;
+  recibirRespuesta() {
+    let respuesta = prompt("Ingresa tu respuesta (solo números del 1 al 4)");
+    respuesta = parseInt(respuesta, 10);
+    while (isNaN(respuesta) || respuesta < 1 || respuesta > 4) {
+      respuesta = prompt(
+        "Respuesta no válida, por favor ingrese nuevamente (solo números del 1 al 4)"
+      );
+      respuesta = parseInt(respuesta, 10);
     }
+    return respuesta;
+  }
 }
 
 class Encuesta {
-    constructor(preguntas) {
-        this.preguntas = preguntas;
-        this.respuestas = [];
-        this.conteos = preguntas.map(() => [0, 0, 0, 0]);
-    }
+  constructor() {
+    this.preguntas = [];
+    this.respuestas = [];
+    this.conteos = [];
+  }
 
-    iniciar() {
-        console.clear();
-        this.respuestas = [];
-        this.preguntas.forEach((pregunta, indice) => {
-            pregunta.mostrarPreguntas();
-            const respuesta = pregunta.recibirRespuesta(); 
-            this.respuestas.push(respuesta);
-            this.conteos[indice][respuesta - 1] += 1;
-        });
-        alert('Ha finalizado la encuesta');
-        document.getElementById('btnResultados').style.display = 'inline-block';
-        document.getElementById('btnConteos').style.display = 'inline-block';
-    }
+  agregarPregunta(pregunta) {
+    this.preguntas.push(pregunta);
+    this.conteos.push([0, 0, 0, 0]);
+  }
 
-    mostrarResultados() {
-        console.log('Resultados de tu encuesta:');
-        this.preguntas.forEach((pregunta, indice) => {
-            console.log(`${pregunta.texto} - Respuesta: ${pregunta.opciones[this.respuestas[indice] - 1]}`);
-        });
-    }
+  iniciar() {
+    console.clear();
+    this.respuestas = [];
+    this.preguntas.forEach((pregunta, indice) => {
+      pregunta.mostrarPregunta();
+      const respuesta = pregunta.recibirRespuesta();
+      this.respuestas.push(respuesta);
+      this.conteos[indice][respuesta - 1] += 1;
+    });
+    alert("Ha finalizado la encuesta");
+    document.getElementById("btnResultados").style.display = "inline-block";
+    document.getElementById("btnConteos").style.display = "inline-block";
+  }
 
-    mostrarConteos() {
-        console.clear();
-        console.log('Conteo de votos:');
-        this.preguntas.forEach((pregunta, indice) => { 
-            console.log(`${pregunta.texto}:`);
-            pregunta.opciones.forEach((opcion, indiceOpcion) => { 
-                console.log(`${opcion}: ${this.conteos[indice][indiceOpcion]} veces`);
-            });
-        });
-    }
+  mostrarResultados() {
+    console.log("Resultados de tu encuesta:");
+    this.preguntas.forEach((pregunta, indice) => {
+      console.log(
+        `${pregunta.texto} - Respuesta: ${
+          pregunta.opciones[this.respuestas[indice] - 1]
+        }`
+      );
+    });
+  }
+
+  mostrarConteos() {
+    console.clear();
+    console.log("Conteo de votos:");
+    this.preguntas.forEach((pregunta, indice) => {
+      console.log(`${pregunta.texto}:`);
+      pregunta.opciones.forEach((opcion, indiceOpcion) => {
+        console.log(`${opcion}: ${this.conteos[indice][indiceOpcion]} veces`);
+      });
+    });
+  }
 }
 
-const preguntas = [
-    new Pregunta("¿Cuál es tu dispositivo tecnológico favorito para el día a día?", ["Smartphone", "Notebook", "Tablet", "Smartwatch"]),
-    new Pregunta("¿Cuál es tu red social favorita?", ["Facebook", "Instagram", "Twitter", "TikTok"]),
-    new Pregunta("¿Qué plataforma de streaming prefieres para ver películas y series?", ["Netflix", "Amazon Prime Video", "Disney+", "HBO Max"]),
-    new Pregunta("¿Cuál es tu género de videojuegos favorito?", ["Acción/Aventura", "RPG (Role-Playing Game)", "FPS (First-Person Shooter)", "Estrategia"]),
-    new Pregunta("¿Qué prefieres usar para escuchar música o podcasts?", ["Auriculares inalámbricos", "Auriculares con cable", "Altavoces Bluetooth", "No suelo utilizar dispositivos de audio"]),
-    new Pregunta("¿Qué tipo de contenido prefieres ver en plataformas de video (YouTube, TikTok, etc.)?", ["Educacional", "Entretenimiento", "Vlogs/personal", "Música"]),
-    new Pregunta("¿Qué tecnología emergente te interesa más?", ["Inteligencia Artificial", "Realidad Virtual/Aumentada", "Internet de las Cosas (IoT)", "Blockchain"]),
-    new Pregunta("¿Cuál es tu plataforma preferida para trabajar o estudiar de forma remota?", ["Zoom", "Microsoft Teams", "Google Meet", "Skype"])
-];
+class EncuestaUI {
+  constructor(encuesta) {
+    this.encuesta = encuesta;
+    this.preguntaIndex = 1;
+    this.maxPreguntas = 4;
+  }
 
-const encuesta = new Encuesta(preguntas);
+  openModal() {
+    document.getElementById("preguntasModal").style.display = "block";
+  }
 
-document.getElementById('btnIniciar').addEventListener('click', () => encuesta.iniciar());
-document.getElementById('btnResultados').addEventListener('click', () => encuesta.mostrarResultados());
-document.getElementById('btnConteos').addEventListener('click', () => encuesta.mostrarConteos());
+  closeModal() {
+    document.getElementById("preguntasModal").style.display = "none";
+  }
+
+  agregarPregunta() {
+    const preguntaInput = document.querySelector(".pregunta").value;
+    const opcionesInputs = document.querySelectorAll(".opciones");
+
+    if (
+      preguntaInput === "" ||
+      [...opcionesInputs].some((opcion) => opcion.value === "")
+    ) {
+      alert("Por favor, rellena todos los campos.");
+      return;
+    }
+
+    const nuevaPregunta = new Pregunta(
+      preguntaInput,
+      [...opcionesInputs].map((opcion) => opcion.value)
+    );
+    this.encuesta.agregarPregunta(nuevaPregunta);
+
+    document.querySelector(".pregunta").value = "";
+    opcionesInputs.forEach((opcion) => (opcion.value = ""));
+
+    const campo = document.querySelector(".preguntasI");
+    campo.textContent = `Pregunta ${this.preguntaIndex + 1}`;
+
+    this.preguntaIndex++;
+
+    if (this.preguntaIndex > this.maxPreguntas) {
+      document.getElementById("btnPreguntas").style.display = "none";
+      document.getElementById("btnIniciar").style.display = "block";
+      alert("Has añadido todas las preguntas.");
+      this.closeModal();
+    }
+  }
+
+  mostrarBotonIniciar() {
+    document.getElementById("btnPreguntas").style.display = "none";
+    document.getElementById("btnIniciar").style.display = "block";
+  }
+}
+
+// Inicializando las clases
+const encuesta = new Encuesta();
+const encuestaUI = new EncuestaUI(encuesta);
+
+// Eventos de botones
+document
+  .querySelector(".continuarModal")
+  .addEventListener("click", () => encuestaUI.agregarPregunta());
+
+document
+  .getElementById("btnIniciar")
+  .addEventListener("click", () => encuesta.iniciar());
+
+document
+  .getElementById("btnResultados")
+  .addEventListener("click", () => encuesta.mostrarResultados());
+
+document
+  .getElementById("btnConteos")
+  .addEventListener("click", () => encuesta.mostrarConteos());
+
+document
+  .getElementById("btnPreguntas")
+  .addEventListener("click", () => encuestaUI.openModal());
